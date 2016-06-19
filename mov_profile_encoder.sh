62437cc6f90f6aeaeac67de7d45fe49072639a9e
Michihiro Okada <olux.888@gmail.com>
2016-06-19 19:40:26 +0900

    Add shell script that search mov file and convert profile.

---
+++ mov_profile_encoder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TARGET_DIR=$1
+#echo ${TARGET_DIR}
+
+MOV_FILES=$(find ${TARGET_DIR} -name "*.MOV")
+#echo ${MOV_FILES}
+
+for MOV_FILE in ${MOV_FILES}
+do
+    if [ ! -f ${MOV_FILE}.org ]; then
+        mv ${MOV_FILE} ${MOV_FILE}.org
+        ffmpeg -i ${MOV_FILE}.org -profile:v baseline -level:v 4.1 -strict -2 ${MOV_FILE}
+    else
+        echo ${MOV_FILE}".org found!"
+    fi
+done
+
