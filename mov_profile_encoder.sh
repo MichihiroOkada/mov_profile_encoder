2526cde90eff7878becef2dafafcc58afc8be9e9
Michihiro Okada <olux.888@gmail.com>
2020-02-01 23:29:06 +0900

    Add change original file timestamp code.

--- mov_profile_encoder.sh
+++ mov_profile_encoder.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $# != 1 ]; then
+    echo "usage: "$0" [Target directory path]"
+    exit
+fi
+
+
 TARGET_DIR=$1
 #echo ${TARGET_DIR}
 
@@ -11,6 +17,8 @@ do
     if [ ! -f ${MOV_FILE}.org ]; then
         mv ${MOV_FILE} ${MOV_FILE}.org
         ffmpeg -i ${MOV_FILE}.org -profile:v baseline -level:v 4.1 -strict -2 ${MOV_FILE}
+
+        touch -r ${MOV_FILE}.org ${MOV_FILE}
     else
         echo ${MOV_FILE}".org found!"
     fi
